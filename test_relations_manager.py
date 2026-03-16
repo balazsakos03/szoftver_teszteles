dd3701f86ec45b92a5582549dcee41e1deddd28f
Balázs Ákos <balazs.akos@maros.emte.ro>
2026-03-16 17:49:02 +0200

    Second test: John Doe's team members

--- test_relations_manager.py
+++ test_relations_manager.py
@@ -22,3 +22,15 @@ def test_john_doe_is_leader_and_birthdate(rm):
     assert john is not None
     assert rm.is_leader(john) is True
     assert john.birth_date == datetime.date(1970, 1, 31)
+
+# 2. Check if John Doe’s team members are Myrta Torkelson and Jettie Lynch
+def test_john_doe_team_members(rm):
+    john = get_employee_by_name(rm, "John", "Doe")
+    member_ids = rm.get_team_members(john) #[2, 3]
+    
+    #id alapu kereses, majd a neveket listaba
+    members = [e for e in rm.get_all_employees() if e.id in member_ids]
+    member_names = [f"{e.first_name} {e.last_name}" for e in members]
+    
+    assert "Myrta Torkelson" in member_names
+    assert "Jettie Lynch" in member_names
