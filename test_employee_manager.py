abd8094ff5dd2816cfe22a19853696352330820f
Balázs Ákos <balazs.akos@maros.emte.ro>
2026-03-23 17:21:17 +0200

    Third test: Salary calculation and email notification testing

--- test_employee_manager.py
+++ test_employee_manager.py
@@ -59,3 +59,28 @@ def test_leader_salary_with_3_members(em, rm):
     salary = em.calculate_salary(leader)
     
     assert salary == 3600
+
+
+# 3. Make sure that when you calculate the salary and send an email notification, 
+# the respective email sender service is used with the correct information.
+@patch('builtins.print') #print fuggveny mockolasa mivel nem kuld emailt
+@patch('employee_manager.datetime.date', MockDate)
+def test_salary_notification(mock_print, em, rm):
+    leader = Employee(
+        id=100, first_name="Boss", last_name="Lady", base_salary=2000,
+        birth_date=datetime.date(1980, 1, 1), 
+        hire_date=datetime.date(2008, 10, 10)
+    )
+    
+    rm.is_leader = MagicMock(return_value=True)
+    rm.get_team_members = MagicMock(return_value=[101, 102, 103])
+    
+    #fizetes szamolasa es "email" kuldese
+    em.calculate_salary_and_send_email(leader)
+    
+    #print meghivas ellenorzes
+    mock_print.assert_called_once()
+    
+    #megfelelo szoveggel lett e meghivva
+    expected_message = "Boss Lady your salary: 3600 has been transferred to you."
+    mock_print.assert_called_with(expected_message)
